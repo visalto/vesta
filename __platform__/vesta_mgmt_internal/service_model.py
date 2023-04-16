2b03ee4824fe3eb50d137c3ee2feb84f10e696d6
Stefan Nikolic <stefannikolic.10s@gmail.com>
2023-04-16 15:11:39 -0500

    saving latest changes

--- __platform__/vesta_mgmt_internal/service_model.py
+++ __platform__/vesta_mgmt_internal/service_model.py
@@ -23,7 +23,8 @@ def _run_cmd(cmd: str, on_success_msg: str, on_error_msg: str, fail_gracefully: 
     else:
         logger.warning(on_error_msg)
         if not fail_gracefully:
-            raise RuntimeError(f'Command failed with exit code {exit_code}. {on_error_msg}')
+            raise RuntimeError(
+                f'Command failed with exit code {exit_code}. {on_error_msg}')
 
 
 class VestaService:
@@ -36,7 +37,8 @@ class VestaService:
         # Run checks for vesta service to make sure
         # it is initialized properly and all underlying
         # components are present
-        checks = [self.check_if_module_exist, self.check_if_docker_compose_exist]
+        checks = [self.check_if_module_exist,
+                  self.check_if_docker_compose_exist]
         for service_check in checks:
             logger.info(f'{service_check.__name__} running...')
             service_check()
@@ -51,7 +53,8 @@ class VestaService:
         compose_file_name = 'docker-compose.yml'
         is_file_present = compose_file_name in os.listdir(self.path)
         if not is_file_present:
-            raise RuntimeError(f'docker-compose not found in service module {self.name}')
+            raise RuntimeError(
+                f'docker-compose not found in service module {self.name}')
 
         logger.info(f'{self.name} docker-compose present')
 
@@ -61,7 +64,8 @@ class VestaService:
                   f"docker-compose --env-file {self.ENV_FILE.__str__()} up -d"
         if force_build:
             command += ' --build'
-        _run_cmd(command, f'{self.name} service started successfully', f'{self.name} error starting up')
+        _run_cmd(command, f'{self.name} service started successfully',
+                 f'{self.name} error starting up')
 
     def stop(self):
         logger.info(f'Stopping service {self.name}')
@@ -69,12 +73,16 @@ class VestaService:
         command = "" \
                   f"cd {self.dir_str} &&" \
                   f"docker-compose --env-file {self.ENV_FILE.__str__()} down"
-        _run_cmd(command, f'{self.name} service stopped successfully', f'Error stopping {self.name}')
+        _run_cmd(command, f'{self.name} service stopped successfully',
+                 f'Error stopping {self.name}')
 
 
 class Vesta:
     VESTAIGNORE_FILE_NAME = '.vestaignore'
     UTIL_SERVICES_SUBMODULE = 'util-services'
+    SERVICES_PRIORITY = ['vesta-ci-cd', 'vesta-dataset-storage',
+                         'vesta-lineage', 'vesta-airflow',
+                         'vesta-git', 'vesta-platform-core']
 
     def __init__(self, base_dir: Path):
         self.BASE_DIR = base_dir
@@ -82,19 +90,22 @@ class Vesta:
         self.services = self.scan_service_list()
 
         if not self.ENV_FILE:
-            raise FileNotFoundError('.env is missing from root Vesta repo. Check this doc for more info')
+            raise FileNotFoundError(
+                '.env is missing from root Vesta repo. Check this doc for more info')
 
     def start_services(self, force_build: bool = False):
         logger.info('Vesta start signal received..')
-        self._logger_util('Parameters summary before deployment process begins')
+        self._logger_util(
+            'Parameters summary before deployment process begins')
         self.check_or_crete_network()
         logger.info('Deploying platform components...')
         for service in self.services:
             service.start(force_build)
         logger.info('All components have been deployed')
-        logo_path = self.BASE_DIR.joinpath('__platform__/vesta_mgmt_internal/vesta_cmd_ascii.txt').resolve()
+        logo_path = self.BASE_DIR.joinpath(
+            '__platform__/vesta_mgmt_internal/vesta_cmd_ascii.txt').resolve()
         if logo_path.exists():
-            os.system(f'type {logo_path.__str__()}')
+            os.system(f'cat {logo_path.__str__()}')
         logger.info('Platform url: {}')
 
     def _logger_util(self, header: str):
@@ -126,26 +137,32 @@ class Vesta:
         return service_list_to_ignore
 
     def scan_service_list(self) -> List[VestaService]:
-        base_dir_list = os.listdir(BASE_DIR)
+        base_dir_list = os.listdir(self.BASE_DIR)
         vesta_ignore = self._parse_vestaignore()
         util_services = self.UTIL_SERVICES_SUBMODULE
         services_list = []
         for root_item in base_dir_list:
             is_util_services = root_item == util_services
-            root_item_path: Path = BASE_DIR.joinpath(root_item)
+            root_item_path: Path = self.BASE_DIR.joinpath(root_item)
             if is_util_services:
-                for util_service in os.listdir(BASE_DIR.joinpath(root_item)):
-                    util_service_path: Path = root_item_path.joinpath(util_service)
+                for util_service in os.listdir(self.BASE_DIR.joinpath(root_item)):
+                    util_service_path: Path = root_item_path.joinpath(
+                        util_service)
                     is_util_service_in_ignore = True in [fnmatch.fnmatch(f'{root_item}/{util_service}', i) for i in
                                                          vesta_ignore]
                     if not is_util_service_in_ignore:
                         services_list.append(util_service_path)
             else:
                 is_item_in_ignore = root_item in vesta_ignore
-                is_glob_pattern_matching = True in [fnmatch.fnmatch(root_item, i) for i in vesta_ignore]
+                is_glob_pattern_matching = True in [
+                    fnmatch.fnmatch(root_item, i) for i in vesta_ignore]
                 if not is_item_in_ignore and not is_glob_pattern_matching:
                     services_list.append(root_item_path)
-        parsed = [VestaService(i, self.BASE_DIR, self.ENV_FILE) for i in services_list]
+        ordered_services = [self.BASE_DIR.joinpath(
+            i) for i in self.SERVICES_PRIORITY]
+        services_list = list(set(ordered_services + services_list))
+        parsed = [VestaService(i, self.BASE_DIR, self.ENV_FILE)
+                  for i in services_list]
         return parsed
 
     def service(self, service_name: str) -> VestaService:
@@ -160,7 +177,8 @@ class Vesta:
         vesta_network = os.environ.get('DOCKER_NETWORK_NAME')
         is_network_present = vesta_network in networks
         if not is_network_present:
-            logger.info(f'Network is missing. Creating {vesta_network} network')
+            logger.info(
+                f'Network is missing. Creating {vesta_network} network')
             client.networks.create(vesta_network)
             logger.info('Network created successfully')
 
@@ -170,11 +188,15 @@ if __name__ == '__main__':
     BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
     platform = Vesta(BASE_DIR)
+    print(platform.ENV_FILE)
     platform.start_services()
-    # git_service = platform.service('vesta-git')
-    # lineage_service = platform.service('vesta-lineage')
-    # lineage_service.stop()
-    # platform_web = platform.service('vesta-platform-core')
-    # platform_web.start()
-    # git_service.start()
-    print('')
+    # vesta_init = platform.service('vesta-ide')
+    # vesta_init.start(force_build=False)
+    # # vesta_init.stop()
+    # # git_service = platform.service('vesta-git')
+    # # lineage_service = platform.service('vesta-lineage')
+    # # lineage_service.stop()
+    # # platform_web = platform.service('vesta-platform-core')
+    # # platform_web.start()
+    # # git_service.start()
+    # print(BASE_DIR)
--- manage.py
+++ manage.py
@@ -4,124 +4,44 @@ from typing import Union, List
 from pathlib import Path
 import logging
 import fnmatch
-
+from __platform__.vesta_mgmt_internal.service_model import Vesta
+import argparse
 logging.basicConfig(level='INFO')
+# todo: Automatically create external network if it doesnt exist 
 
 logger = logging.getLogger(__name__)
 VESTA = 'vesta'
 BASE_DIR = Path(__file__).resolve().parent
 ENV_FILE = BASE_DIR.joinpath('.env')
+platform = Vesta(BASE_DIR)
+parser = argparse.ArgumentParser(description='Vesta management')
+parser.add_argument('--env', '-e', help='environment file', default=ENV_FILE)
+parser.add_argument('command', choices=[
+                    'start', 'stop', 'init'], help='Commands to interact with the system')
 
 
-def _parse_vestaignore(base_dir: Path = BASE_DIR) -> List[str]:
-    vestaignore = '.vestaignore'
-    service_list_to_ignore = []
-    with open(base_dir.joinpath(vestaignore)) as f:
-        for service in f:
-            service = service.strip()
-            if not service or service.startswith('#'):
-                continue
-
-            service_list_to_ignore.append(service)
-    return service_list_to_ignore
+def run_init():
+    vesta_init = platform.service('vesta-init')
+    vesta_init.start()
 
 
-def fetch_list_of_services(services_as_string: bool = False) -> Union[List[Path], List[str]]:
-    base_dir_list = os.listdir(BASE_DIR)
-    vesta_ignore = _parse_vestaignore()
-    util_services = 'util-services'
-    services_list = []
-    for root_item in base_dir_list:
-        is_util_services = root_item == util_services
-        root_item_path: Path = BASE_DIR.joinpath(root_item)
-        if is_util_services:
-            for util_service in os.listdir(BASE_DIR.joinpath(root_item)):
-                util_service_path: Path = root_item_path.joinpath(util_service)
-                is_util_service_in_ignore = True in [fnmatch.fnmatch(f'{root_item}/{util_service}', i) for i in
-                                                     vesta_ignore]
-                if not is_util_service_in_ignore:
-                    services_list.append(util_service_path)
-        else:
-            is_item_in_ignore = root_item in vesta_ignore
-            is_glob_pattern_matching = True in [fnmatch.fnmatch(root_item, i) for i in vesta_ignore]
-            if not is_item_in_ignore and not is_glob_pattern_matching:
-                services_list.append(root_item_path)
-    if services_as_string:
-        parsed = [i.__str__() for i in services_list]
-        return parsed
-    return services_list
+def start():
+    platform.start_services()
 
 
-@click.command()
-@click.argument('service', type=str)
-@click.option('--mode', type=str, default=None)
-@click.option('--build', default=False, is_flag=True, flag_value=True,
-              help='Force to rebuild the image upon starting the containers')
-@click.option('-f', default=False, is_flag=True, flag_value=True,
-              help='Follow logs output')
-@click.option('--branch', default=None, type=str)
-def service_manager(service: str, mode: str = None,
-                    build: bool = False, f: bool = False,
-                    branch: str = None):
-    is_service_present = service in fetch_list_of_services(True)
-    is_git_request = service == 'git'
-    if is_git_request:
-        from __platform__.vesta_mgmt_internal.checkout_new_branch import new_branch
-        new_branch(branch)
-    else:
-        is_addressing_platform = service == 'vesta'
-
-        MODE_ALLOWED_OPTIONS = ['start', 'stop', 'logs']
-        if mode not in MODE_ALLOWED_OPTIONS:
-            raise ValueError(f'Invalid mode options. Options: {tuple(MODE_ALLOWED_OPTIONS)}')
-        # change working directory to the service
-        os.chdir(BASE_DIR.joinpath(service))
-        is_start = mode == 'start'
-        is_stop = mode == 'stop'
-        is_logs = mode == 'logs'
-        if is_start:
-            start_service_docker_compose(service, build)
-        if is_stop:
-            stop_service_docker_compose(service)
-        if is_logs:
-            show_service_logs(f)
+def stop():
+    platform.stop_services()
 
 
-def _run_cmd(cmd: str, on_success_msg: str, on_error_msg: str) -> None:
-    exit_code = os.system(cmd)
-    if exit_code == 0:
-        logger.info(on_success_msg)
-    else:
-        logger.warning(on_error_msg)
-        raise RuntimeError(f'Command failed with exit code {exit_code}')
-    pass
-
-
-def show_service_logs(follow: bool) -> None:
-    docker_cmd = f'docker-compose --env-file {ENV_FILE.__str__()} logs'
-    if follow:
-        docker_cmd += ' -f'
-    _run_cmd(docker_cmd, 'Logs Fetched', 'Error fetching logs')
-
-
-def start_service_docker_compose(service_name: str, build: bool):
-    logger.info(f'Starting service {service_name}')
-    # format docker compose command
-    docker_cmd = f'docker-compose --env-file {ENV_FILE.__str__()} up -d'
-    if build:
-        logger.info('   Build flag detected. Will rebuild the image if there are changes')
-        docker_cmd += ' --build'
-
-    logger.info(f'running command: {docker_cmd}')
-    _run_cmd(docker_cmd, 'Service started successfully', f'Error starting {service_name}')
-
-
-def stop_service_docker_compose(service_name: str):
-    logger.info(f'Stopping service {service_name}')
-    # format docker compose command
-    docker_cmd = f'docker-compose --env-file {ENV_FILE.__str__()} down'
-    _run_cmd(docker_cmd, 'Service stopped successfully', f'Error stopping {service_name}')
+def main():
+    args = parser.parse_args()
+    if args.command == 'init':
+        run_init()
+    if args.command == 'start':
+        start()
+    if args.command == 'stop':
+        stop()
 
 
 if __name__ == '__main__':
-    service_manager()
+    main()
